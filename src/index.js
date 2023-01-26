54899ba34df828f43c87076e57e17dbdc2f99c8f
Lennert Vandecasteele <lennert.vandecasteele@outlook.com>
2023-01-26 22:27:00 +0000

    Install binance lib and test connection

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+const { Spot } = require("@binance/connector");
 const config = require("./config");
+
+const client = new Spot(config.api.key, config.api.secret);
+
+client
+	.accountSnapshot("SPOT")
+	.then((response) => console.log(response.data.snapshotVos));
 
 console.log(config);
