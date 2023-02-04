f18737733e5b89515dac3d33ebcc45bff492d63a
Lennert Vandecasteele <lennert.vandecasteele@outlook.com>
2023-02-04 20:56:45 +0000

    Add test client

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,28 @@
 require("dotenv").config();
 
 const config = {
+	env: process.env.NODE_ENV,
 	api: {
 		key: process.env.API_KEY,
 		secret: process.env.API_SECRET,
 	},
+	testApi: {
+		key: process.env.TESTNET_API_KEY,
+		secret: process.env.TESTNET_API_SECRET,
+	},
+	fiat: "EUR",
+	trades: [
+		{
+			asset: "BTC",
+			currency: "USDT",
+			currencyQty: 30,
+		},
+		{
+			asset: "ETH",
+			currency: "USDT",
+			currencyQty: 20,
+		},
+	],
 };
 
 module.exports = config;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const { Spot } = require("@binance/connector");
 const config = require("./config");
 
-const client = new Spot(config.api.key, config.api.secret);
-
-client
-	.accountSnapshot("SPOT")
-	.then((response) => console.log(response.data.snapshotVos));
-
-console.log(config);
+(async () => {
+	const client = new Spot(config.api.key, config.api.secret);
+	const testClient =
+		config.env === "production"
+			? null
+			: new Spot(config.testApi.key, config.testApi.secret, { baseURL: "https://testnet.binance.vision" });
+})();
