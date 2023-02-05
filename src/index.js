a939adcf625e9cf74adbdd85f73a700b4224ae51
Lennert Vandecasteele <lennert.vandecasteele@outlook.com>
2023-02-05 14:15:19 +0000

    Place orders for each trade

--- src/index.js
+++ src/index.js
@@ -5,33 +5,18 @@ const options = config.env === "production" ? {} : { baseURL: "https://testnet.b
 const client = new Spot(config.api.key, config.api.secret, options);
 
 (async () => {
-	const fiatAsset = config.env === "production" ? config.fiat.asset : "BNB";
-	const currency = config.env === "production" ? config.currency : "USDT";
-	const fiat = await getAsset(fiatAsset);
+	const fiat = await getAsset(config.fiat.asset);
 	if (fiat.free >= config.fiat.amount) {
-		const symbol = fiatAsset + currency;
+		const symbol = config.fiat.asset + config.currency;
 
 		const order = await client.newOrder(symbol, "BUY", "MARKET", { quantity: config.fiat.amount });
 		const currencyQty = order.data.cummulativeQuoteQty;
-		console.log(currencyQty);
-
-		// const exchangeInfo = await client.exchangeInfo();
-		// console.log(exchangeInfo.data.symbols[0].filters);
 
 		config.trades.forEach(async (trade) => {
 			const quoteOrderQty = floor((trade.percentage / 100) * currencyQty, 2);
-			console.log(quoteOrderQty);
-			const symbol = trade.asset + currency;
-			try {
-				const order = await client.newOrder(symbol, "BUY", "MARKET", { quoteOrderQty });
-				console.log(order.data);
-			} catch (error) {
-				console.log(error);
-			}
+			const symbol = trade.asset + config.currency;
+			await client.newOrder(symbol, "BUY", "MARKET", { quoteOrderQty });
 		});
-
-		const account = await client.account();
-		console.log(account.data.balances);
 	}
 })();
 
